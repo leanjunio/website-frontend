483c1b149f2dcd7ce7a188510ce2b244a3577d16
Lean Junio <leanjunio@live.com>
2020-02-13 20:17:39 -0500

    create ContactForm create AboutPage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
+import ContactForm from './ContactForm';
 
-const HomePage = () => (
-  <div className="container-fluid">
-    <Navbar />
-  </div>
-);
+const HomePage = () => {
+  return (
+    <div className="container-fluid">
+      <Navbar />
+      <h1 className="title">Hi!</h1>
+      <p className="subtitle">My name is Lean, thanks for visiting! Feel free to browse around!</p>
+      <ContactForm />
+    </div>
+  );
+};
 
 export default HomePage;
